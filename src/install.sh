d8d1af99de17a4223b21ee60e7c60810ae94b68e
beginor <beginor@qq.com>
2019-03-11 06:50:47 +0800

    zip file name

--- src/install.sh
+++ src/install.sh
@@ -5,50 +5,59 @@ set -e
 # update && install require packages
 apt-get update
 mkdir -p /usr/share/man/man1
-apt-get install -y --no-install-recommends curl unzip fonts-noto-cjk openjdk-8-jre-headless procps
+apt-get install -y --no-install-recommends curl unzip fonts-noto-cjk openjdk-8-jre-headless procps ca-certificates
 
 # geoserver
-curl --location --output ~/geoserver.zip https://build.geoserver.org/geoserver/release/2.15.0/geoserver-2.15.0-bin.zip
-unzip ~/geoserver.zip -d / && mv -v /geoserver* /geoserver
-rm ~/geoserver.zip
+curl --location --output ~/geoserver-2.15.0.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/geoserver-2.15.0-bin.zip
+unzip ~/geoserver-2.15.0.zip -d / && mv -v /geoserver-2.15 /geoserver
+rm ~/geoserver-2.15.0.zip
 
 # geoserver-mysql-plugin
-curl --location --output ~/geoserver-mysql-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-mysql-plugin.zip
-unzip -o ~/geoserver-mysql-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-mysql-plugin.zip
+curl --location --output ~/geoserver-2.15.0-mysql-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-mysql-plugin.zip
+unzip -o ~/geoserver-2.15.0-mysql-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-mysql-plugin.zip
 
 # sqlserver-plugin
-curl --location --output ~/geoserver-sqlserver-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-sqlserver-plugin.zip
-unzip -o ~/geoserver-sqlserver-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-sqlserver-plugin.zip
+curl --location --output ~/geoserver-2.15.0-sqlserver-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-sqlserver-plugin.zip
+unzip -o ~/geoserver-2.15.0-sqlserver-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-sqlserver-plugin.zip
 
 # oracle-plugin
-curl --location --output ~/geoserver-oracle-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-oracle-plugin.zip
-unzip -o ~/geoserver-oracle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-oracle-plugin.zip
+curl --location --output ~/geoserver-2.15.0-oracle-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-oracle-plugin.zip
+unzip -o ~/geoserver-2.15.0-oracle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-oracle-plugin.zip
 
 # css-plugin
-curl --location --output ~/geoserver-css-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-css-plugin.zip
-unzip -o ~/geoserver-css-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-css-plugin.zip
+curl --location --output ~/geoserver-2.15.0-css-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-css-plugin.zip
+unzip -o ~/geoserver-2.15.0-css-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-css-plugin.zip
 
 # ysld-plugin
-curl --location --output ~/geoserver-ysld-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-ysld-plugin.zip
-unzip -o ~/geoserver-ysld-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-ysld-plugin.zip
+curl --location --output ~/geoserver-2.15.0-ysld-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-ysld-plugin.zip
+unzip -o ~/geoserver-2.15.0-ysld-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-ysld-plugin.zip
 
 # pyramid-plugin
-curl --location --output ~/geoserver-pyramid-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-pyramid-plugin.zip
-unzip -o ~/geoserver-pyramid-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-pyramid-plugin.zip
+curl --location --output ~/geoserver-2.15.0-pyramid-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-pyramid-plugin.zip
+unzip -o ~/geoserver-2.15.0-pyramid-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-pyramid-plugin.zip
 
 # vectortiles-plugin
-curl --location --output ~/geoserver-vectortiles-plugin.zip https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-vectortiles-plugin.zip
-unzip -o ~/geoserver-vectortiles-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-vectortiles-plugin.zip
+curl --location --output ~/geoserver-2.15.0-vectortiles-plugin.zip \
+  https://build.geoserver.org/geoserver/release/2.15.0/plugins/geoserver-2.15.0-vectortiles-plugin.zip
+unzip -o ~/geoserver-2.15.0-vectortiles-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.15.0-vectortiles-plugin.zip
 
 # mbstyle-plugin
-curl --location --output ~/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip https://build.geoserver.org/geoserver/2.15.x/community-latest/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip
+curl --location --output ~/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip \
+  https://build.geoserver.org/geoserver/2.15.x/community-latest/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip
 unzip -o ~/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
 rm ~/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip
 
@@ -56,6 +65,6 @@ rm ~/geoserver-2.15-SNAPSHOT-mbstyle-plugin.zip
 mv /tmp/web.xml /geoserver/webapps/geoserver/WEB-INF/web.xml
 
 # cleanup
-apt-get purge -y unzip
+apt-get purge -y unzip ca-certificates
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
