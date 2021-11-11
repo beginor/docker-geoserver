649de24a87e9a252255e44f9cc32e89925f2fe7b
beginor <beginor@qq.com>
2021-11-11 12:03:59 +0800

    2.19.3

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache --rm -t beginor/geoserver:2.19.1 .
+docker build --no-cache --rm -t beginor/geoserver:2.19.3 .
--- src/install.sh
+++ src/install.sh
@@ -7,58 +7,58 @@ mkdir -p /usr/share/man/man1
 apt-get install -y --no-install-recommends curl unzip fonts-noto-cjk openjdk-11-jre-headless procps
 
 # geoserver
-curl --location --output ~/geoserver-2.19.1.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/geoserver-2.19.1-bin.zip
-unzip ~/geoserver-2.19.1.zip -d /geoserver
-rm ~/geoserver-2.19.1.zip
+curl --location --output ~/geoserver-2.19.3.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/geoserver-2.19.3-bin.zip
+unzip ~/geoserver-2.19.3.zip -d /geoserver
+rm ~/geoserver-2.19.3.zip
 
 # geoserver-mysql-plugin
-curl --location --output ~/geoserver-2.19.1-mysql-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-mysql-plugin.zip
-unzip -o ~/geoserver-2.19.1-mysql-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-mysql-plugin.zip
+curl --location --output ~/geoserver-2.19.3-mysql-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-mysql-plugin.zip
+unzip -o ~/geoserver-2.19.3-mysql-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-mysql-plugin.zip
 
 # sqlserver-plugin
-curl --location --output ~/geoserver-2.19.1-sqlserver-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-sqlserver-plugin.zip
-unzip -o ~/geoserver-2.19.1-sqlserver-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-sqlserver-plugin.zip
+curl --location --output ~/geoserver-2.19.3-sqlserver-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-sqlserver-plugin.zip
+unzip -o ~/geoserver-2.19.3-sqlserver-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-sqlserver-plugin.zip
 
 # oracle-plugin
-curl --location --output ~/geoserver-2.19.1-oracle-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-oracle-plugin.zip
-unzip -o ~/geoserver-2.19.1-oracle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-oracle-plugin.zip
+curl --location --output ~/geoserver-2.19.3-oracle-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-oracle-plugin.zip
+unzip -o ~/geoserver-2.19.3-oracle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-oracle-plugin.zip
 
 # css-plugin
-curl --location --output ~/geoserver-2.19.1-css-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-css-plugin.zip
-unzip -o ~/geoserver-2.19.1-css-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-css-plugin.zip
+curl --location --output ~/geoserver-2.19.3-css-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-css-plugin.zip
+unzip -o ~/geoserver-2.19.3-css-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-css-plugin.zip
 
 # ysld-plugin
-curl --location --output ~/geoserver-2.19.1-ysld-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-ysld-plugin.zip
-unzip -o ~/geoserver-2.19.1-ysld-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-ysld-plugin.zip
+curl --location --output ~/geoserver-2.19.3-ysld-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-ysld-plugin.zip
+unzip -o ~/geoserver-2.19.3-ysld-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-ysld-plugin.zip
 
 # pyramid-plugin
-curl --location --output ~/geoserver-2.19.1-pyramid-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-pyramid-plugin.zip
-unzip -o ~/geoserver-2.19.1-pyramid-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-pyramid-plugin.zip
+curl --location --output ~/geoserver-2.19.3-pyramid-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-pyramid-plugin.zip
+unzip -o ~/geoserver-2.19.3-pyramid-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-pyramid-plugin.zip
 
 # vectortiles-plugin
-curl --location --output ~/geoserver-2.19.1-vectortiles-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-vectortiles-plugin.zip
-unzip -o ~/geoserver-2.19.1-vectortiles-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-vectortiles-plugin.zip
+curl --location --output ~/geoserver-2.19.3-vectortiles-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-vectortiles-plugin.zip
+unzip -o ~/geoserver-2.19.3-vectortiles-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-vectortiles-plugin.zip
 
 # mbstyle-plugin
-curl --location --output ~/geoserver-2.19.1-mbstyle-plugin.zip \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.1/extensions/geoserver-2.19.1-mbstyle-plugin.zip
-unzip -o ~/geoserver-2.19.1-mbstyle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
-rm ~/geoserver-2.19.1-mbstyle-plugin.zip
+curl --location --output ~/geoserver-2.19.3-mbstyle-plugin.zip \
+  http://sourceforge.net/projects/geoserver/files/GeoServer/2.19.3/extensions/geoserver-2.19.3-mbstyle-plugin.zip
+unzip -o ~/geoserver-2.19.3-mbstyle-plugin.zip -d /geoserver/webapps/geoserver/WEB-INF/lib/
+rm ~/geoserver-2.19.3-mbstyle-plugin.zip
 
 # copy cors enabled web.xml
 mv /tmp/web.xml /geoserver/webapps/geoserver/WEB-INF/web.xml
